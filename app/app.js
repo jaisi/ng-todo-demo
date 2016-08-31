6b479d5574c39f9d98580514fa34914c100d60c7
Joe Shepherd <joeshepmedia@gmail.com>
2016-08-30 22:52:47 -0500

    Update routes version for D15; add clearer insrux comments

--- app/app.js
+++ app/app.js
@@ -1,63 +1,20 @@
 "use strict";
 
-var app = angular.module("TodoApp", []);
+var app = angular.module("TodoApp", ["ngRoute"]);
 
-app.controller("NavCtrl", function($scope){
-	$scope.navItems = [{name: "Logout"}, {name: "All Items"}, {name:"New Item"}];
-});
-
-app.controller("TodoCtrl", function($scope){
-	$scope.welcome = "hello";
-  $scope.showListView = true;
-  $scope.newTask = {};
-  $scope.items = [
-    {
-      id: 0,
-      task: "mow the lawn",
-      isCompleted: true,
-      dueDate: "12/5/17",
-      assignedTo: "greg",
-      location: "Zoe's house",
-      urgency: "low",
-      dependencies: "sunshine, clippers, hat, water, headphones"
-    },
-    {
-      id: 1,
-      task: "grade quizzes",
-      isCompleted: false,
-      dueDate: "12/5/15",
-      assignedTo: "Joe",
-      location: "NSS",
-      urgency: "high",
-      dependencies: "wifi, tissues, vodka"
-    },
-    {
-      id: 2,
-      task: "take a nap",
-      isCompleted: false,
-      dueDate: "5/21/16",
-      assignedTo: "zoe",
-      location: "Zoe's house",
-      urgency: "medium",
-      dependencies: "hammock, cat, pillow, blanket"
-    }
-  ];
-
-  $scope.newItem = function(){
-      console.log("you clicked new Item");
-      $scope.showListView = false;
-  };
-
-  $scope.allItem = function(){
-    console.log("you clicked all items");
-    $scope.showListView = true;
-  };
-
-  $scope.addNewItem = function(){
-    $scope.newTask.isCompleted = false;
-    $scope.newTask.id = $scope.items.length;
-    console.log("you added a new item", $scope.newTask);
-    $scope.items.push($scope.newTask);
-    $scope.newTask = {};
-  };
+app.config(function($routeProvider){
+  $routeProvider.
+    when('/items/list',{
+      templateUrl: 'partials/item-list.html',
+      controller: 'ItemListCtrl'
+    }).
+    when('/items/new', {
+      templateUrl: 'partials/item-new.html',
+      controller: 'ItemNewCtrl'
+    }).
+    when('/items/details', {
+      templateUrl: 'partials/item-details.html',
+      controller: "ItemViewCtrl"
+    }).
+    otherwise('/items/list');
 });
